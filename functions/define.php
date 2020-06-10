e996bd5869fb90db1b3d57bb266e62df256f19eb
pariz_externo <adpariz@gmail.com>
2020-06-09 21:16:28 -0300

    add new folders

---
+++ functions/define.php
@@ -0,0 +1,8 @@
+
+<?php 
+ 	define('BASE_URL', 'https://alexdavid2019-mp-ecommerce-php.herokuapp.com/');
+  	define('ADMIN_BASE_URL', 'https://alexdavid2019-mp-ecommerce-php.herokuapp.com/admin');
+  	define('MY_COOKIE','SBwi72UCKlwiqzz2abcd7892jnxzb');//aleatoreo
+ 	define('MY_COOKIE_EXPIRE', time()+(86400 *30));//30days //in second
+	define('PLATFORM_ID','BR8GRBKHPKU001JJH7F0');
+ ?>
---
+++ inc/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=1024">
+    <title>Tienda e-commerce</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="./assets/favicon.ico">
+	
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+		integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+		crossorigin="anonymous"></script>
+	
+    <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
+    <link rel="stylesheet" href="./assets/category.css" media="screen, print">
+    <link rel="stylesheet" href="./assets/merch-tools.css" media="screen, print">
+    <link rel="stylesheet" href="./assets/fonts" media="">
+	
+    <!-- Bootstrap core CSS v3.3.7 -->
+    <link href="<?= BASE_URL ;?>/assets/bootstrap.min.css" rel="stylesheet">
+	
+    <link href="<?= BASE_URL ;?>/assets/main.css" rel="stylesheet">
+	
+    <!--<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">  -->
+    <!--font-awesome.min.css 41 New Icons in 4.7-->
+    <link href="<?= BASE_URL ;?>/assets/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<script src="<?=BASE_URL;?>/assets/bootstrap.min.js"></script>
+
+</head>
+
+<body class="as-theme-light-heroimage">
